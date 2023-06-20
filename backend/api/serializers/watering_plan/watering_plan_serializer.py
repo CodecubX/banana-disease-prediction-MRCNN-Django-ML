02f25a8109b51010764c426f1e3b4e7a037ac67a
avishkakavindu <avishkakavindud@gmail.com>
2023-06-20 20:37:29 +0530

    Watering plan serializer changes

--- backend/api/serializers/watering_plan/watering_plan_serializer.py
+++ backend/api/serializers/watering_plan/watering_plan_serializer.py
@@ -6,6 +6,8 @@ from api.models.watering_plan import WateringPlan
 class WateringPlanSerializer(serializers.ModelSerializer):
     """ Serializes the WateringPlan model objects and return selected fields """
 
+    watering_plan = serializers.CharField(source='get_watering_plan_display')
+
     class Meta:
         model = WateringPlan
         fields = '__all__'
