652cf7dc6a5b16a438c77c0e9d6c6ed49498c066
avishkakavindu <avishkakavindud@gmail.com>
2023-06-24 21:22:10 +0530

    Watering plan response changed

--- backend/api/serializers/watering_plan/watering_plan_serializer.py
+++ backend/api/serializers/watering_plan/watering_plan_serializer.py
@@ -7,9 +7,14 @@ class WateringPlanSerializer(serializers.ModelSerializer):
     """ Serializes the WateringPlan model objects and return selected fields """
 
     watering_plan = serializers.CharField(source='get_watering_plan_display')
-    stage = serializers.CharField(source='get_stage_display')
     variety = serializers.StringRelatedField()
 
     class Meta:
         model = WateringPlan
         fields = '__all__'
+
+    def to_representation(self, instance):
+        """Add stage_display field to the serialized representation"""
+        representation = super().to_representation(instance)
+        representation['stage'] = instance.get_stage_display()
+        return representation
