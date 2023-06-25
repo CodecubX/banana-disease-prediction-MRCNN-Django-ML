1e991b11a1d743a3cc5352c4181523a687bb0700
avishkakavindu <avishkakavindud@gmail.com>
2023-06-25 16:31:35 +0530

    Symptom description fields added to disease model

--- backend/api/api_views/disease/cure_api_view.py
+++ backend/api/api_views/disease/cure_api_view.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import RetrieveAPIView, ListAPIView
+from rest_framework.generics import ListAPIView
 from rest_framework import permissions
 
 from api.models import Cure
