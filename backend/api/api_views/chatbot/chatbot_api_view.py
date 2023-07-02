27c6c2108843315c42bec87e1ed2774c66ab8e25
avishkakavindu <avishkakavindud@gmail.com>
2023-07-02 12:27:06 +0530

    Code refactored

--- backend/api/api_views/chatbot/chatbot_api_view.py
+++ backend/api/api_views/chatbot/chatbot_api_view.py
@@ -1,16 +1,9 @@
-import pandas as pd
-
 from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from api.models import Disease
-
-from api.serializers import DiseaseSerializer
-
 from api.utils import ChatBot
-from api.utils.chatbot import symptom_based
-from .utils.utils import build_model_data
+from .utils.utils import build_model_data, handle_chatbot_response
 
 
 class ChatBotAPIView(APIView):
@@ -32,66 +25,16 @@ class ChatBotAPIView(APIView):
         """
         msg, tag_from_req, language = build_model_data(request.data, request.query_params)
 
-        model = ChatBot(language=language, mode='development')
+        try:
+            model = ChatBot(language=language, mode='development')
+        except Exception as e:
+            print(f'ERROR: {e}')
+            return Response({'error': 'Something went wrong with the chatbot'}, status=status.HTTP_409_CONFLICT)
 
         context = {
             'response': '',
             'language': language,
             'tag': tag_from_req
         }
-
-        if tag_from_req != 'identify_diseases_by_symptoms':
-            intent_predictions = model.get_predictions(msg)
-
-            tag = intent_predictions[0]['intent']
-            response = model.get_response(tag)
-
-            if language == 'si':
-                # encode the Sinhala text using UTF-8 encoding
-                response = response.encode('utf-8')
-
-            context['response'] = response
-            context['tag'] = tag
-
-            # returns response and all diseases in the db for the dropdown
-            if tag == 'banana_disease_info' or tag == 'management_strategies':
-                diseases = Disease.objects.all()
-
-                serializer = DiseaseSerializer(
-                    diseases,
-                    fields=['id', 'name', 'name_display'],
-                    many=True
-                )
-                context['diseases'] = serializer.data
-
-        else:
-            if language == 'en':
-                disease_data = Disease.objects.values('name', 'symptom_description')
-                df = pd.DataFrame.from_records(disease_data)
-                # ! TODO make columns dynamic
-                df = df.rename(columns={'name': 'Disease/Pest', 'symptom_description': 'Description'})
-
-                predicted_diseases = symptom_based.find_top_k_diseases(
-                    symptoms=msg,
-                    df=df,
-                    k=3,
-                    verbose=True
-                )
-                print(f'INFO: Predicted Diseases: {predicted_diseases}')
-                # ! TODO improve following
-                diseases = []
-
-                for disease in predicted_diseases:
-                    disease_name = disease[0]
-                    obj = Disease.objects.get(name=disease_name)
-                    payload = {
-                        'id': obj.id,
-                        'name_display': obj.get_name_display(),
-                        'confidence': disease[1]
-                    }
-                    diseases.append(payload)
-
-                context['diseases'] = diseases
-                context['response'] = 'Those are the diseases that fits the description'
-
+        context = handle_chatbot_response(tag_from_req, msg, model, context, language='en')
         return Response(context, status=status.HTTP_200_OK, content_type='text/plain; charset=utf-8')
