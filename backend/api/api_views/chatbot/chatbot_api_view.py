b61dab6e2cf2d5c48387e5c6d38e425476a020b2
avishkakavindu <avishkakavindud@gmail.com>
2023-07-06 21:45:58 +0530

    Responses added

--- backend/api/api_views/chatbot/chatbot_api_view.py
+++ backend/api/api_views/chatbot/chatbot_api_view.py
@@ -36,7 +36,11 @@ class ChatBotAPIView(APIView):
             'language': language,
             'tag': tag_from_req
         }
-        context = handle_chatbot_response(tag_from_req, msg, model, context, language=language)
+
+        try:
+            context = handle_chatbot_response(tag_from_req, msg, model, context, language=language)
+        except IndexError:
+            return Response({'error': 'Chat bot responses not found'}, status=status.HTTP_409_CONFLICT)
         if language == 'si':
             return Response(context, status=status.HTTP_200_OK, content_type='text/plain; charset=utf-8')
         return Response(context, status=status.HTTP_200_OK)
