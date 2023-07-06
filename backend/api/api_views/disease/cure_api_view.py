217c4cf5b46749b20202527059266e8de57872bb
avishkakavindu <avishkakavindud@gmail.com>
2023-07-06 22:06:13 +0530

    Content type added

--- backend/api/api_views/disease/cure_api_view.py
+++ backend/api/api_views/disease/cure_api_view.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListAPIView
 from rest_framework import permissions
+from rest_framework.response import Response
 
 from api.models import Cure
 from api.serializers import CureSerializer, CureSinhalaSerializer
@@ -28,3 +29,14 @@ class CureAPIView(ListAPIView):
             return CureSinhalaSerializer
 
         return CureSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        language = request.query_params.get('language', None)
+
+        if language == 'si':
+            # Set the content type to 'text/plain; charset=utf-8'
+            return Response(serializer.data, content_type='text/plain; charset=utf-8')
+        else:
+            return Response(serializer.data)
