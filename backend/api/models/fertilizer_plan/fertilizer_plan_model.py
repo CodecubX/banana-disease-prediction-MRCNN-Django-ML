73ac544ceaf44372ea83432bfb3ea99cd8249c10
avishkakavindu <avishkakavindud@gmail.com>
2023-07-11 21:04:36 +0530

    __str__ method updated

--- backend/api/models/fertilizer_plan/fertilizer_plan_model.py
+++ backend/api/models/fertilizer_plan/fertilizer_plan_model.py
@@ -22,4 +22,4 @@ class FertilizerPlan(AbstractBananaPlant):
         unique_together = ('variety', 'stage', 'fertilizer_type')
 
     def __str__(self):
-        return f'{self.get_fertilizer_type_display()}: {self.stage} | {self.fertilizer_type}'
+        return f'{self.get_fertilizer_type_display()}: {self.stage} | {self.variety}'
