c21974f15905702c46f74fdefbf3c50395ccda67
avishkakavindu <avishkakavindud@gmail.com>
2023-08-30 20:56:37 +0530

    Migration changes

--- backend/api/models/disease/disease_prediction_model.py
+++ backend/api/models/disease/disease_prediction_model.py
@@ -12,7 +12,7 @@ class DiseasePrediction(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
     # fields for prediction fields
-    disease = models.ForeignKey(Disease, on_delete=models.DO_NOTHING, null=False, blank=False)
+    disease = models.ForeignKey(Disease, on_delete=models.CASCADE, null=False, blank=False)
     probabilities = models.JSONField(null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
